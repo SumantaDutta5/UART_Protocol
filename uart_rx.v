6033552a4f80b6210d5e04d9ba39b13322ebe461
Sumanta Kumar Dutta <84955195+SumantaDutta5@users.noreply.github.com>
2026-06-14 18:28:07 +0530

    Implement UART receiver module

---
+++ uart_rx.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+
+module uart_reciever(input clk,rst,rx,rdy_clr,clken,output reg  rdy,output reg [7:0] data_out);
+
+always@(posedge clk)
+    if(rst) begin
+        rdy = 0;
+        data_out = 0;
+    end
+        
+parameter RX_STATE_START = 2'b00;
+parameter RX_STATE_data_out = 2'b01;
+parameter RX_STATE_STOP = 2'b10;
+
+reg [1:0] state = RX_STATE_START;
+reg [3:0] sample = 0;
+reg [3:0] index = 0;
+reg [7:0] temp = 8'b0;
+
+always @(posedge clk) begin
+	if (rdy_clr)	rdy <= 0;
+
+	if (clken) begin
+	case (state)
+	
+	RX_STATE_START: begin
+	/*
+	* Start counting from the first low sample;
+	* Once we've sampled a full bit, start collecting data_out bits.
+	*/
+		if (!rx || sample != 0)	sample <= sample + 4'b1;
+		
+		if (sample == 15) begin
+			state <= RX_STATE_data_out;
+			index <= 0;
+			sample <= 0;
+			temp <= 0;
+		end
+	end
+
+	RX_STATE_data_out: begin
+		sample <= sample + 4'b1;
+		if (sample == 4'h8) begin
+			temp[index] <= rx;
+			index <= index + 4'b1;
+		end
+
+		if (index == 8 && sample == 15)	state <= RX_STATE_STOP;
+	end
+
+	RX_STATE_STOP: begin
+	/*
+	* Our baud clock may not be running at exactly the same rate as the transmitter.
+	* If we think that we're at least half-way into the stop bit,
+	* allow transition into handling the next start bit.
+	*/
+		if (sample == 15 ) begin
+			state <= RX_STATE_START;
+			data_out <= temp;
+			rdy <= 1'b1;
+			sample <= 0;
+		end
+
+		else	sample <= sample + 4'b1;
+	end
+
+	default: state <= RX_STATE_START;
+	
+	endcase
+	end
+end
+
+endmodule
