9c9812a31f5f95c2ce6225cef853a414407c84b2
Sumanta Kumar Dutta <84955195+SumantaDutta5@users.noreply.github.com>
2026-06-14 18:29:21 +0530

    Add UART top module with sender and receiver integration

---
+++ uart_top.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+`include "baud_rate_generator.v"
+`include "uart_sender.v"
+`include "uart_rx.v"
+
+module uart_top (input rst,input [7:0] data_in,input wr_en,clk,input rdy_clr,output rdy,busy,output [7:0] data_out);
+
+wire rx_clk_en;  //collecting output of baud rate generator rx_enb signal
+wire tx_clk_en;  //connectiong output of baud rate generator tx_enb signal
+
+wire tx_temp;    //connecting the output of tx module
+
+baud_rate_generator bg(clk,rst,tx_clk_en,rx_clk_en);
+
+uart_sender us(clk,wr_en,tx_clk_en,rst,data_in,tx_temp,busy);
+
+uart_reciever ur(clk,rst,tx_temp,rdy_clr,rx_clk_en,rdy,data_out);
+
+endmodule
